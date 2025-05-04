a0e6edb8099364d2370c2f9d8762e61f55d95d74
PBEM22 <dlacofbs1229@gmail.com>
2025-05-05 00:02:28 +0900

    #20 [feat] admin login 기능 추가 및 axios 전역 관리 생성

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -8,4 +8,8 @@ const index = () => {
   )
 }
 
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+axios.defaults.withCredentials = true;
+
 export default index
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,14 @@ import { usePathname } from 'next/navigation';
 import Header from '@/components/layout/header/Header';
 import Home from './home/home';
 import "../scss/styles.scss"; 
+import axios from 'axios';
 
 export default function Page() {
   const pathname = usePathname();
   const isMainPage = pathname === '/';
-
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+  axios.defaults.withCredentials = true;
+  
   return (
     <div className="page-container">
       {!isMainPage && <Header />}
