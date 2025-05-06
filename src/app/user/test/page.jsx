7d0bfefbb2e2ebcf8c6b85dfd81e842011fe8999
정희수 <gmltn3780@naver.com>
2025-05-06 16:24:15 +0900

    [feat, style] user test 통신연결, stepper style로 변경

--- src/app/user/test/page.jsx
+++ src/app/user/test/page.jsx
@@ -1,101 +1,137 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../../scss/styles.scss";
 import Navigate from "@/components/layout/navigate/Navigate";
-import { ButtonDafult, NextBtn, PrevBtn } from "@/components/common/Button";
+import { ButtonDafult } from "@/components/common/Button";
 
 import { Button, MobileStepper, useTheme } from "@mui/material";
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
-
-const questions = [
-  {
-    question: "아침에 눈 떴을 때 당신의 반응은?",
-    answers: ["5분만 더...", "기상! 상쾌함", "알람 끔", "SNS 확인"],
-  },
-  {
-    question: "아침 식사는?",
-    answers: ["절대 안 먹음", "꼭 먹음", "시간 나면", "카페에서"],
-  },
-];
+import api from "@/utils/axios";
+import { useRouter } from "next/navigation";
 
 const TestMain = () => {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const router = useRouter();
+    const theme = useTheme();
+    const [questions, setQuestions] = useState([]);
+    const [activeStep, setActiveStep] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-  const handleNextStep = () => {
-    if (selectedAnswer !== null) {
-      setSelectedAnswer(null);
-      setActiveStep((prev) => prev + 1);
-    }
-  };
+    const handleNextStep = (answerIdx) => {
+        setSelectedAnswer(answerIdx);
+        setTimeout(() => {
+            setSelectedAnswer(null);
+            if (activeStep === questions.length - 1) {
+                router.push("/user/test/result");
+            } else {
+                setActiveStep((prev) => prev + 1);
+            }
+        }, 200);
+    };
 
-  const handleBack = () => {
-    setActiveStep((prev) => prev - 1);
-  };
+    const handleBack = () => {
+        setActiveStep((prev) => prev - 1);
+    };
 
-  const currentQuestion = questions[activeStep];
+    const currentQuestion = questions[activeStep];
 
-  return (
-    <div className="inner">
-      <div className="gauge-bar"></div>
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            try {
+                const res = await api.get("/v1/questions");
+                setQuestions(res.data.data);
+                console.log("질문 리스트", res.data.data);
+            } catch (error) {
+                console.error("가져오기 실패", error);
+            }
+        };
+        fetchQuestions();
+    }, []);
+    if (!currentQuestion) return null;
 
-      <Navigate title="MBTI는 끝났다. 이제는 VBPI" step={`${activeStep + 1}/${questions.length}`} />
+    return (
+        <div className="inner">
+            <Navigate title="MBTI는 끝났다. 이제는 VBPI" step={`${activeStep + 1}/${questions.length}`} />
 
-      {/* 질문 */}
-      <div className="question-title">{currentQuestion.question}</div>
+            {/* 질문 */}
+            <div className="question-title">{currentQuestion.questionInfo}</div>
 
-      {/* 답변 */}
-      <div className="answer-wrapper">
-        {currentQuestion.answers.map((text, idx) => (
-          <ButtonDafult
-            key={idx}
-            backgroundColor={selectedAnswer === idx ? "var(--primary)" : "var(--white)"}
-            color={selectedAnswer === idx ? "var(--white)" : "var(--black)"}
-            onClick={() => setSelectedAnswer(idx)}
-          >
-            {text}
-          </ButtonDafult>
-        ))}
-      </div>
+            {/* 답변 */}
+            <div className="answer-wrapper">
+                {currentQuestion.answers.map((a, idx) => (
+                    <ButtonDafult
+                        key={a.answerId}
+                        style={{
+                            backgroundColor: selectedAnswer === idx ? "var(--primary)" : "var(--white)",
+                            color: selectedAnswer === idx ? "var(--white)" : "var(--black)",
+                        }}
+                        onClick={() => handleNextStep(idx)}
+                    >
+                        {a.answer}
+                    </ButtonDafult>
+                ))}
+            </div>
 
-      {/* Stepper */}
-      <MobileStepper
-        sx={{
-          maxWidth: 400,
-          flexGrow: 1,
-          mt: 3,
-          backgroundColor: "transparent",
-        }}
-        variant="progress"
-        steps={questions.length}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNextStep}
-            disabled={activeStep === questions.length - 1 || selectedAnswer === null}
-          >
-            Next
-            {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            Back
-          </Button>
-        }
-      />
+            {/* Stepper */}
+            <MobileStepper
+                sx={{
+                    maxWidth: 500,
+                    // flexGrow: 1,
+                    padding: 0,
+                    backgroundColor: "transparent",
+                    "& .MuiLinearProgress-root": {
+                        height: 4,
+                        // borderRadius: 4,
+                        backgroundColor: "var(--gray-20)", // 배경 선 색상
+                    },
+                    "& .MuiLinearProgress-bar": {
+                        backgroundColor: "var(--primary)", // 진행된 선 색상
+                    },
+                }}
+                variant="progress"
+                steps={questions.length}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                    <Button
+                        size="small"
+                        onClick={() => handleNextStep(selectedAnswer)}
+                        disabled={activeStep === questions.length - 1 || selectedAnswer === null}
+                        sx={{
+                            color: "var(--primary)",
 
-      {/* 이전, 다음 버튼 */}
-      <div className="prev-btn">
-        <PrevBtn onClick={handleBack} />
-        <NextBtn disabled={selectedAnswer === null} onClick={handleNextStep} />
-      </div>
-    </div>
-  );
+                            "&:hover": {
+                                backgroundColor: "rgb(97 173 196 / 22%)",
+                                color: "var(--primary)",
+                            },
+                        }}
+                    >
+                        Next
+                        {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                    </Button>
+                }
+                backButton={
+                    <Button
+                        size="small"
+                        onClick={handleBack}
+                        disabled={activeStep === 0}
+                        sx={{
+                            color: "var(--primary)",
+                            "&:hover": {
+                                backgroundColor: "rgb(97 173 196 / 22%)",
+                                color: "var(--primary)",
+                            },
+                            "&disables": {
+                                color: "rgba(0, 0, 0, 0.26)",
+                            },
+                        }}
+                    >
+                        {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                        Back
+                    </Button>
+                }
+            />
+        </div>
+    );
 };
 
 export default TestMain;
