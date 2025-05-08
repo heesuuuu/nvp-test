265a7887f47405908690020e994fbc4fdf655fbd
정희수 <gmltn3780@naver.com>
2025-05-09 01:24:18 +0900

    [style] nvp 홈페이지 레이아웃 수정

--- src/app/user/nvp/page.jsx
+++ src/app/user/nvp/page.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import "../../../scss/styles.scss"
+"use client";
+import React from "react";
+import "../../../scss/styles.scss";
+import Navigate from "@/components/layout/navigate/Navigate";
 
 const NvpHome = () => {
-  return (
-      <div className="nvp-wrapper">
-          <div>
-              <img
-                  src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Saluting%20Face.png"
-                  alt="Saluting Face"
-                  width="125"
-                  height="125"
-              />
-          </div>
-          <div className='nvp-title'>
-              눈 깜빡하면 돌아올게요 😉
-              <p> NVP 홈페이지는 2학기 출시 예정입니다!</p>
-          </div>
-      </div>
-  );
-}
+    return (
+        <div className="nvp-big-wrapper">
+            <div className="inner">
+                <Navigate title="Comming Soon" />
+                <div className="nvp-wrapper">
+                    <div>
+                        <img
+                            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Saluting%20Face.png"
+                            alt="Saluting Face"
+                            width="125"
+                            height="125"
+                        />
+                    </div>
+                    <div className="nvp-title">
+                        눈 깜빡하면 돌아올게요 😉
+                        <p> NVP 홈페이지는 2학기 출시 예정입니다!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default NvpHome
+export default NvpHome;
